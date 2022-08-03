fc6b70a363943fcae38cae15acaf2f0c1610d323
dawidkpy <dawid_k@wp.pl>
2022-08-03 11:52:59 +0200

    V1

--- folder1/program.py
+++ folder1/program.py
@@ -1,2 +1,31 @@
 
-print("nic nie rozumiem")
+def dekor(f):
+
+    def w():
+
+        print('!!!Witamy w Naszym salonie!!!')
+        return f()
+
+    return w
+
+def check_brand(list):
+    pass
+
+def check_model(list):
+    pass
+
+def buy_car():
+    pass
+
+@dekor
+def main():
+    pass
+
+if __name__ == '__main__':
+
+        brand = ['AUDI', 'BMW']
+        modelAudi = {'A3': 10, 'A4': 12}
+        modelBMW = {'3': 5, '5': 5}
+
+        main()
+
