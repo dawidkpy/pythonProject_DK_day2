aa037b5df9cdef2f40b94266becf8369bba445d9
dawidkpy <dawid_k@wp.pl>
2022-08-04 14:28:47 +0200

    V1

--- folder3/main.py
+++ folder3/main.py
@@ -8,12 +8,11 @@ with open("my_csv.csv", encoding="utf-8") as file:
     moj_csv = csv.DictReader(file)
 
     for r in moj_csv:
-        # key = r["Lp"]
-        # slownik[key] = r
         slownik.append(r)
 
 print(slownik)
 
 with open("my_json.json", "w", encoding="utf-8") as file2:
     file2.write(json.dumps(slownik, indent=4))
+    #file2.write(json.dumps(slownik))
 
